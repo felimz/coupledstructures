b044b082e7b41ebf779660a76d5d5b488f5069c1
Felipe Mejia <felimz@gmail.com>
2018-09-19 10:11:22 -0500

    Added computer check

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+from os import chdir
+from sys import exit
 import sap2000
 from collections import OrderedDict
 from modelclasses import Props
 
-#%% OPEN SAP2000 AND READY PROGRAM
+# %% OPEN SAP2000 AND READY PROGRAM
 
 # check working directory
-api_path = r'C:\Users\Felipe\Dropbox\PycharmProjects\CoupledStructures\models'
+api_path_home = r'C:\Users\Felipe\Dropbox\PycharmProjects\CoupledStructures\models'
+api_path_fluor = r'C:\Users\mej12981\PycharmProjects\CoupledStructures\models'
+try:
+    chdir(api_path_home)
+except FileNotFoundError:
+    try:
+        chdir(api_path_fluor)
+    except FileNotFoundError:
+        exit(1)
+    else:
+        api_path = api_path_fluor
+else:
+    api_path = api_path_home
+
 sap2000.checkinstall(api_path)
 
 # create sap2000 object in memory
 attach_to_instance = False
-specify_path = False
+specify_path = False  # program finds most recent installation after SAP2000 v19. For other installs, specify path.
 program_path = r'C:\Program Files\Computers and Structures\SAP2000 20\SAP2000.exe'
 my_sap_object = sap2000.attachtoapi(attach_to_instance, specify_path, program_path)
 
@@ -23,7 +38,7 @@ sap2000.newmodel(sap_model)
 
 #%% DEFINE MODEL GEOMETRY, PROPERTIES, AND LOADING
 
-# initiatilize frame properties object
+# initialize frame properties object
 props_obj = Props()
 
 # set degrees of freedom for 2-D motion into sap2000
@@ -49,31 +64,31 @@ sap_model.SetPresentUnits(sap2000.UNITS['kip_ft_F'])
 
 # add frame object by coordinates
 
-[Frame1, ret] = sap_model.FrameObj.AddByCoord(0, 0, 0, 0, 0, 20,  '', 'R1', '', 'Global')
-[Frame2, ret] = sap_model.FrameObj.AddByCoord(20, 0, 0, 20, 0, 20, '', 'R1', '', 'Global')
-[Frame3, ret] = sap_model.FrameObj.AddByCoord(0, 0, 20, 20, 0, 20, '', 'R2', '', 'Global')
-[Frame4, ret] = sap_model.FrameObj.AddByCoord(40, 0, 0, 40, 0, 20, '', 'R3', '', 'Global')
-[Frame5, ret] = sap_model.FrameObj.AddByCoord(60, 0, 0, 60, 0, 20, '', 'R3', '', 'Global')
-[Frame6, ret] = sap_model.FrameObj.AddByCoord(40, 0, 20, 60, 0, 20, '', 'R4', '', 'Global')
+[Frame1] = sap_model.FrameObj.AddByCoord(0, 0, 0, 0, 0, 20,  '', 'R1', '', 'Global')
+[Frame2] = sap_model.FrameObj.AddByCoord(20, 0, 0, 20, 0, 20, '', 'R1', '', 'Global')
+[Frame3] = sap_model.FrameObj.AddByCoord(0, 0, 20, 20, 0, 20, '', 'R2', '', 'Global')
+[Frame4] = sap_model.FrameObj.AddByCoord(40, 0, 0, 40, 0, 20, '', 'R3', '', 'Global')
+[Frame5] = sap_model.FrameObj.AddByCoord(60, 0, 0, 60, 0, 20, '', 'R3', '', 'Global')
+[Frame6] = sap_model.FrameObj.AddByCoord(40, 0, 20, 60, 0, 20, '', 'R4', '', 'Global')
 
 
 # assign point object restraint at base
 
 Restraint = [True, True, True, True, True, True]
 
-[Frame1i, Frame1j, ret] = sap_model.FrameObj.GetPoints(Frame1, '', '')
+[Frame1i, Frame1j] = sap_model.FrameObj.GetPoints(Frame1, '', '')
 
 sap_model.PointObj.SetRestraint(Frame1i, Restraint)
 
-[Frame2i, Frame1j, ret] = sap_model.FrameObj.GetPoints(Frame2, '', '')
+[Frame2i, Frame1j] = sap_model.FrameObj.GetPoints(Frame2, '', '')
 
 sap_model.PointObj.SetRestraint(Frame2i, Restraint)
 
-[Frame4i, Frame4j, ret] = sap_model.FrameObj.GetPoints(Frame4, '', '')
+[Frame4i, Frame4j] = sap_model.FrameObj.GetPoints(Frame4, '', '')
 
 sap_model.PointObj.SetRestraint(Frame4i, Restraint)
 
-[Frame5i, Frame5j, ret] = sap_model.FrameObj.GetPoints(Frame5, '', '')
+[Frame5i, Frame5j] = sap_model.FrameObj.GetPoints(Frame5, '', '')
 
 sap_model.PointObj.SetRestraint(Frame5i, Restraint)
 
@@ -90,12 +105,12 @@ sap_model.LoadPatterns.Add(load_patterns[2], sap2000.LOAD_PATTERN_TYPES[load_pat
 
 # assign loading for load pattern 'D'
 
-[Frame3i, Frame3j, ret] = sap_model.FrameObj.GetPoints(Frame3, '', '')
+[Frame3i, Frame3j] = sap_model.FrameObj.GetPoints(Frame3, '', '')
 sap_model.PointObj.SetLoadForce(Frame3i, load_patterns[1], [0, 0, -10, 0, 0, 0])
 sap_model.PointObj.SetLoadForce(Frame3j, load_patterns[1], [0, 0, -10, 0, 0, 0])
 
 # assign loading for load pattern 'EQ'
-[Frame3i, Frame3j, ret] = sap_model.FrameObj.GetPoints(Frame3, '', '')
+[Frame3i, Frame3j] = sap_model.FrameObj.GetPoints(Frame3, '', '')
 sap_model.FrameObj.SetLoadDistributed(Frame3, load_patterns[2], 1, 10, 0, 1, 1.8, 1.8)
 
 # switch to k-in units
@@ -111,7 +126,7 @@ sap2000.saveandrunmodel(sap_model, api_path, file_name)
 
 # initialize for Sap2000 results
 
-[Frame3i, Frame3j, ret] = sap_model.FrameObj.GetPoints(Frame3, '', '')
+[Frame3i, Frame3j] = sap_model.FrameObj.GetPoints(Frame3, '', '')
 
 # get Sap2000 results for all load patterns:
 
@@ -123,7 +138,7 @@ for key, val in load_patterns.items():
 
     sap_model.Results.Setup.SetCaseSelectedForOutput(val)
 
-    [NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3, ret] = sap_model.Results.JointDispl(
+    [NumberResults, Obj, Elm, ACase, StepType, StepNum, U1, U2, U3, R1, R2, R3] = sap_model.Results.JointDispl(
         Frame3i, 0, 0, [], [], [], [], [], [], [], [], [], [], [])
 
     SapResult[val] = U1[0]
